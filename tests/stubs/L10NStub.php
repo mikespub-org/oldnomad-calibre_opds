afefc055a3b9932d6f53c8c524a50abb3b7a4589
Alec Kojaev <alec@kojaev.name>
2025-05-21 15:33:12 +0300

    Tests: allow L10N stub select the display language.

--- tests/stubs/L10NStub.php
+++ tests/stubs/L10NStub.php
@@ -11,9 +11,9 @@ use OCP\IL10N;
 trait L10NStub {
 	protected IL10N $l;
 
-	protected function initL10N(): void {
+	protected function initL10N(?string $pref_locale = null): void {
 		$l = $this->createStub(IL10N::class);
-		$locale = locale_get_default();
+		$locale = $pref_locale ?? locale_get_default();
 		$l->method('getLocaleCode')->willReturn($locale);
 		$l->method('getLanguageCode')->willReturn(locale_get_display_language($locale));
 		$l->method('t')->willReturnCallback(function (string $text, $parameters = []): string {

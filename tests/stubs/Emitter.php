fb97698371ac5d5d0d3cc9c84a55fe61366c1776
Alec Kojaev <alec@kojaev.name>
2025-05-21 15:37:47 +0300

    Tests: add fake Emitter interface for IRootFolder mock

---
+++ tests/stubs/Emitter.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+// SPDX-FileCopyrightText: 2025 Alec Kojaev <alec@kojaev.name>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OC\Hooks;
+
+// Fake interface to satisfy IRootFolder
+interface Emitter {
+}
